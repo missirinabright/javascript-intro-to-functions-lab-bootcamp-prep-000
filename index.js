239e521a71a6118d1ae0633f17c491317d66b92c
Irina Bright <missirinabright@gmail.com>
2019-04-13 00:58:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,19 +18,3 @@ function sayHiToGrandma(string) {
   return "I can't hear you!";
 }
 
-function sayHiToGrandma(string) {
-  var noAnswer = "I can't hear you!";
-  var yesAnswer = "YES INDEED!"
-  var lovUnswer = "I love you, too.";
-  if (string.toLowerCase === string) {
-  return noAnswer;
-}
-  else if (string.toUpperCase === string) {
-  return yesAnswer;
-}
-
-  else if ("I love you, Grandma." === string) {
-    return lovUnswer;
-  }
-}
-
