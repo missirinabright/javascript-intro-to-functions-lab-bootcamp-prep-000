9b50c6daeeb1c209a251dae35e9e501f4727de1b
Irina Bright <missirinabright@gmail.com>
2019-04-13 00:56:11 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!";
+}
+
+function sayHiToGrandma(string) {
+  var noAnswer = "I can't hear you!";
+  var yesAnswer = "YES INDEED!"
+  var lovUnswer = "I love you, too.";
+  if (string.toLowerCase === string)
+  return noAnswer;
+}
+else if (string.toUpperCase === string) {
+  
+}
+
+else { 
+
+
